0ecd8ca7b0c951addc1531c421dcd6877f7606b2
dannypernik <dannypernik@mail.com>
2024-10-13 23:50:29 -0500

    test

--- app/tasks.py
+++ app/tasks.py
@@ -1,5 +1,11 @@
+import os
 from celery import Celery
 from app.create_report import create_sat_score_report, send_pdf_score_report, delete_spreadsheet
+import logging
+
+
+info_file_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'logs/info.log')
+logging.basicConfig(filename=info_file_path, level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 app = Celery('tasks', broker='redis://localhost:6379/0')
 
