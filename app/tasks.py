254ce68d1d93609e027eb69def1bcb5136123101
dannypernik <dannypernik@mail.com>
2024-10-25 15:24:12 -0500

    edit tasks

--- app/tasks.py
+++ app/tasks.py
@@ -4,15 +4,15 @@ from app.create_report import create_sat_score_report, send_pdf_score_report
 from app.email import send_fail_mail, send_task_fail_mail, send_report_submitted_email
 import logging
 
-class MyTaskBaseClass(celery.Task):
-    def on_failure(self, exc, task_id, args, kwargs, einfo):
-        # exc (Exception) - The exception raised by the task.
-        # args (Tuple) - Original arguments for the task that failed.
-        # kwargs (Dict) - Original keyword arguments for the task that failed.
-        send_task_fail_mail(exc, task_id, args, kwargs, einfo)
+# class MyTaskBaseClass(celery.Task):
+#     def on_failure(self, exc, task_id, args, kwargs, einfo):
+#         # exc (Exception) - The exception raised by the task.
+#         # args (Tuple) - Original arguments for the task that failed.
+#         # kwargs (Dict) - Original keyword arguments for the task that failed.
+#         send_task_fail_mail(exc, task_id, args, kwargs, einfo)
 
 @celery.task(name='app.tasks.create_and_send_sat_report', bind=True, autoretry_for=(Exception,), retry_backoff=5, retry_kwargs={'max_retries': 7})
-def create_and_send_sat_report(score_data):
+def create_and_send_sat_report(self, score_data):
   try:
     spreadsheet_id = create_sat_score_report(score_data)
     send_pdf_score_report(spreadsheet_id, score_data)
@@ -22,7 +22,7 @@ def create_and_send_sat_report(score_data):
     raise e
 
 @celery.task(name='app.tasks.send_report_submitted_task', bind=True, autoretry_for=(Exception,), retry_backoff=5, retry_kwargs={'max_retries': 7})
-def send_report_submitted_task(score_data):
+def send_report_submitted_task(self, score_data):
   try:
     send_report_submitted_email(score_data)
     logging.info('Report submitted email sent')
