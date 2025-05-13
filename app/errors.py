001df34b74365d1051eb71506823b766ae3bf634
dannypernik <dannypernik@mail.com>
2025-05-13 16:54:46 -0500

    error page updates

--- app/errors.py
+++ app/errors.py
@@ -9,3 +9,11 @@ def not_found_error(error):
 def internal_error(error):
     db.session.rollback()
     return render_template('500.html'), 500
+
+@app.errorhandler(502)
+def server_error(e):
+    # Log the error for debugging purposes
+    app.logger.error(f"502 Error: {e}")
+
+    # Render a custom error page
+    return render_template('502.html'), 502
