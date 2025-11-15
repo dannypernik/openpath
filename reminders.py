8c8de8778d0a24823ff9b4f805154b6eca66c833
dannypernik <dannypernik@mail.com>
2025-11-14 22:25:11 -0600

    fix

--- reminders.py
+++ reminders.py
@@ -24,6 +24,7 @@ import traceback
 import logging
 import time
 import app.utils as utils
+from memory_profiler import profile
 
 # Configure logging
 error_file_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'logs/errors.log')
