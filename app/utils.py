c47666066eea908e8070602a9271c78856cea4e3
dannypernik <dannypernik@mail.com>
2026-02-17 01:01:03 -0600

    vcard fix

--- app/utils.py
+++ app/utils.py
@@ -370,7 +370,7 @@ def generate_vcard(contacts):
 VERSION:3.0
 N:{contact.get('last_name', '')};{contact.get('first_name', '')};;;
 FN:{contact.get('first_name', '')} {contact.get('last_name', '')}
-EMAIL:{contact.get('email', '')}
+EMAIL;TYPE=HOME:{contact.get('email', '')}
 ORG:{contact.get('role', '').title()}
 TEL;TYPE=CELL:{contact.get('phone', '')}
 NOTE:Timezone: {contact.get('timezone', '')}
