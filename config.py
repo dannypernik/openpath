ff5507dd931e635e7b0b480a81235560c89c6d84
dannypernik <dannypernik@mail.com>
2026-03-02 17:24:55 -0600

    rm unused config var

--- config.py
+++ config.py
@@ -15,7 +15,6 @@ class Config(object):
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ALTCHA_SITE_KEY = os.environ.get('ALTCHA_SITE_KEY')
     ALTCHA_SECRET_KEY = os.environ.get('ALTCHA_SECRET_KEY')
     HCAPTCHA_SITE_KEY = os.environ.get('HCAPTCHA_SITE_KEY')
